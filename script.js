17ced712b76a4002991e2bee6399b92fd5cb85e1
mehmet-eventogy <mehmet@eventogy.com>
2024-05-08 11:45:28 +0100

    completed random message generator

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+ArrOne = [
+  "You",
+  "Your",
+  "You",
+  "Your",
+  "You",
+  "Your",
+  "You",
+  "Your",
+  "You",
+  "Your",
+];
+ArrTwo = ["1", "2", "3", "1", "2", "3", "1", "2", "3", "1"];
+ArrThree = [
+  "Age",
+  "Years",
+  "Age",
+  "Years",
+  "Age",
+  "Years",
+  "Age",
+  "Years",
+  "Age",
+  "Years",
+];
+
+console.log(
+  ArrOne[Math.floor(Math.random() * 10)] +
+    " " +
+    ArrTwo[Math.floor(Math.random() * 10)] +
+    " " +
+    ArrThree[Math.floor(Math.random() * 10)]
+);
